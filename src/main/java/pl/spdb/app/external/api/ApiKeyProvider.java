4bd8bba6c359a4f9f9974be75aa42a493d92065b
ACharchula <antoni.charchula@gmail.com>
2020-05-01 20:34:42 +0200

    fix

--- src/main/java/pl/spdb/app/external/api/ApiKeyProvider.java
+++ src/main/java/pl/spdb/app/external/api/ApiKeyProvider.java
@@ -1,6 +1,6 @@
 package pl.spdb.app.external.api;
 
 public class ApiKeyProvider {
-    public static final String google_api_key = "AIzaSyAXoZwb6pallCCwleHBDiBgM6eVw7spmbI";
+    public static final String google_api_key = "AIzaSyCHwoPo9VLs4Gd27kicOsqkVZsnZ_PklLM";
     public static final String tomtom_api_key = "wVMZXKCPNKVnU0ETrRm8aWSqz2Dpa1bG";
 }
